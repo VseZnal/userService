f9dea0da42e1cdb136f4bdbdd4569c82795e6869
VseZnal <Vseznal66@gmail.com>
2023-04-05 16:38:42 +0300

    chore: new env + config

--- backend/services/user-service/config/config.go
+++ backend/services/user-service/config/config.go
@@ -10,22 +10,25 @@ var (
 	pgConnStringEnvName    = "PG_CONNECTION_STRING"
 	jwtSecret              = "JWT_SECRET"
 	refreshSecret          = "REFRESH_SECRET"
+	refreshPasswordToken   = "REFRESH_PASSWORD_TOKEN"
 )
 
 type Config struct {
-	HostUser     string
-	PortUser     string
-	PgConnString string
-	Jwt          string
-	Refresh      string
+	HostUser        string
+	PortUser        string
+	PgConnString    string
+	Jwt             string
+	Refresh         string
+	RefreshPassword string
 }
 
 func UserConfig() *Config {
 	return &Config{
-		HostUser:     utils.TrimEnv(userServiceHostEnvName),
-		PortUser:     utils.TrimEnv(userServicePortEnvName),
-		PgConnString: utils.TrimEnv(pgConnStringEnvName),
-		Jwt:          utils.TrimEnv(jwtSecret),
-		Refresh:      utils.TrimEnv(refreshSecret),
+		HostUser:        utils.TrimEnv(userServiceHostEnvName),
+		PortUser:        utils.TrimEnv(userServicePortEnvName),
+		PgConnString:    utils.TrimEnv(pgConnStringEnvName),
+		Jwt:             utils.TrimEnv(jwtSecret),
+		Refresh:         utils.TrimEnv(refreshSecret),
+		RefreshPassword: utils.TrimEnv(refreshPasswordToken),
 	}
 }
